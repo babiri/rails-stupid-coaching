64df68fa79982128760f72ee03e7b91a7fb173c7
Pedro Silva <pedrofjrsilva@gmail.com>
2019-05-13 14:46:26 +0100

    defined controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # get 'question/ask'
+  # get 'questions/answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get :ask, to: 'questions#ask'
+  get :answer, to: 'questions#answer'
 end
